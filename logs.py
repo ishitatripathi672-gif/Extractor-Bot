9f04906a56597e4d6765fb7ab52ee108592a9613
Ishita Tripathi <ishitatripathi672@gmail.com>
2026-01-08 21:41:34 +0530

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,22 @@
+# Don't Remove Credit Tg - https://t.me/J1T3SH
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@TeamLeakageUnOfficial
+# Ask Doubt on telegram https://t.me/J1T3SH
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format=
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+logging = logging.getLogger()
+
---
+++ vars.py
@@ -0,0 +1,20 @@
+
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "25158505"))
+API_HASH = environ.get("API_HASH", "902aabefb27ed4c0e330e39f8fd6c559")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8504136547:AAHV5MgebRSQfRGx0Nannva7oDwuAgeK-XU")
+
+# Force Subscribe Configuration
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/NOOB_CBSE")  # Channel link
+
+# Admin Configuration
+ADMINS = list(map(int, environ.get("ADMINS", "6963567931").split()))
+
+# Optional: Bot Owner ID
+OWNER_ID = int(environ.get("OWNER_ID", "6963567931"))
+
+# Database URL (if you want to add database support later)
+DATABASE_URL = environ.get("DATABASE_URL", "")
